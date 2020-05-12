0a04c17adc0af4438e7c768adf01f01ad48a1982
cduyarli <cduyarli@icloud.com>
2020-05-12 18:18:44 +0200

    update

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -2,6 +2,7 @@ package day_1;
 
 public class CreateChannel {
     public static void main(String[] args) {
+        //start with run
         System.out.println("User is able to crate the channels");
     }
 }
